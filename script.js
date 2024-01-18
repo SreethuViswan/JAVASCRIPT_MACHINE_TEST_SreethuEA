98a4dd9a1c36ce2f22e649147d98bc48a3c522f7
SreethuViswan <sreethuv0007@gmail.com>
2024-01-18 11:30:34 +0530

    commit

---
+++ script.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function () {
+const loginForm = document.getElementById("login-form");
+const SignupForm = document.getElementById("signup-form");
+    
+    SignupForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+       
+        const username = document.getElementById("signup-username").value;
+        const password = document.getElementById("signup-password").value;
+
+        if (!localStorage.getItem(username)) {
+            const user = {
+                username: username,
+                password: password,
+            };
+            localStorage.setItem(username, JSON.stringify(user));
+            alert("Account created successfully.You Can now log in");
+        } else {
+            alert("Username already exists. Please choose another.");
+        }
+        SignupForm.reset();
+        
+    });
+
+    loginForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+        const username = document.getElementById("login-username").value;
+        const password = document.getElementById("login-password").value;
+        const storedUser = JSON.parse(localStorage.getItem(username));
+
+        if (storedUser && storedUser.password === password) {
+            alert("Login successful!");
+        } else {
+            alert("Login failed. Please check your credentials.");
+        }
+
+        loginForm.reset();
+    });
+
+});
